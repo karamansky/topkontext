1f23152771e84f2eef278a05e447b6277f6670af
Stas <karamanskyi@SKPro.local>
2023-12-10 22:30:56 +0200

    Added footer copy

--- wp-content/themes/topkontext/footer.php
+++ wp-content/themes/topkontext/footer.php
@@ -1,3 +1,30 @@
+<?php
+	$copy = get_field('footer_copy', 'option');
+	$footer_image = get_field('footer_image', 'option');
+?>
+
+<footer class="footer">
+	<div class="wrapper">
+		<div class="footer__inner">
+
+		</div>
+	</div>
+</footer>
+
+<div class="footer__copy">
+	<div class="wrapper">
+		<div class="footer__copy-inner">
+			<?php if( !empty($copy) ) { ?>
+				<div class="footer__copy-left"><?php echo '&copy;' . date('Y') . ' ' . $copy ?></div>
+			<?php } ?>
+			<?php if( !empty($footer_image) ) { ?>
+				<div class="footer__copy-right">
+					<?php DisplayGlobal::renderAcfImage($footer_image); ?>
+				</div>
+			<?php } ?>
+		</div>
+	</div>
+</div>
 
 <?php wp_footer(); ?>
 
