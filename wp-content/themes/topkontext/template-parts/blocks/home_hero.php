db4c566d65e815853bdf1fd597a9e62fb8fec565
Karamansky <karamansky@gmail.com>
2023-12-16 14:56:15 +0200

    Added hero block

--- wp-content/themes/topkontext/template-parts/blocks/home_hero.php
+++ wp-content/themes/topkontext/template-parts/blocks/home_hero.php
@@ -5,31 +5,35 @@
 		if( GutenbergBlocks::checkForPreview($block) ) return;
 	}
 
-	$background = DisplayGlobal::generateStyleWithBgImageOrNothing( get_field('background_image') );
 	$title = get_field('title');
-	$text = get_field('text');
-	$form = get_field('form_shortcode');
-	$mobile_link = get_field('mobile_link');
+	$description = get_field('description');
+	$button = get_field('button');
+	$image = get_field('image');
+	$guaranty_image = get_field('guaranty_image');
 
-	if( empty($form) && empty($title) && empty($text) ) return false;
+	if( empty($title) && empty($image) ) return false;
 ?>
-<div class="hero" <?php echo $background; ?> >
-	<div class="gj-wrapper">
+<section class="hero" >
+<!--	<div class="wrapper">-->
 		<div class="hero__inner">
 			<div class="hero__content">
 				<?php if( !empty($title) ) { ?>
 					<h1 class="hero__title"><?php echo $title ?></h1>
 				<?php } ?>
-				<?php if( !empty($text) ){ ?>
-					<div class="hero__text"><?php echo $text; ?></div>
+				<?php if( !empty($description) ){ ?>
+					<div class="hero__description"><?php echo $description; ?></div>
+				<?php } ?>
+				<?php DisplayGlobal::renderAcfLink( $button, 'btn btn--blue hero__btn' ) ?>
+			</div>
+			<div class="hero__image">
+				<?php if( !empty($image) ){  DisplayGlobal::renderAcfImage($image, ''); } ?>
+
+				<?php if( !empty($guaranty_image) ) { ?>
+					<div class="hero__guaranty" <?php echo DisplayGlobal::generateStyleWithBgImageOrNothing($guaranty_image); ?>>
+					</div>
 				<?php } ?>
 			</div>
 
-			<div class="hero__form">
-				<?php echo do_shortcode( $form ); ?>
-			</div>
-
-			<?php DisplayGlobal::renderAcfLink( $mobile_link, 'hero__mob-btn gj-btn btn--yellow btn--big' ) ?>
 		</div>
-	</div>
-</div>
+<!--	</div>-->
+</section>
